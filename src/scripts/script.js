626a177f0869a8ecdc87bbdfbb1d022c24c45b8c
João Amado <joaowebpoa@gmail.com>
2023-02-08 15:50:45 -0300

    remove: js libs, add: css transform animations

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,100 +1,15 @@
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
+    const textBox = document.querySelector(".text-box");
+    const bg = document.querySelector(".bg");
+    const overlay = document.querySelector(".overlay");
+
     loader.className += " hidden"; // class "loader hidden"
-});
-
-gsap.registerPlugin(ScrollTrigger);
-ScrollTrigger.defaults({
-    toggleActions: "play complete reverse repeat"
-});
-
-
-gsap.fromTo(".overlay",
-    {
-        y: 200
-    },
-    {
-
-        y: 0,
-        duration: 1,
-        scale: 0.8,
-        transformOrigin: "right bottom",
-        scrollTrigger: {
-            trigger: "#section-intro",
-            pin: true,
-            start: "top top",
-            scrub: true,
-            // markers: {
-            //     startColor: "#a240cc",
-            //     endColor: "#348ed8",
-            //     fontSize: "12px"
-            // }
-
-        }
-
-    });
-
-
-gsap.to(".bg",
-
-    {
-        duration: 2,
-        scale: 1.5,
-        scrollTrigger: {
-            trigger: "body",
-            start: "top top",
-            scrub: true,
-        }
-
-
-    });
-
-gsap.fromTo(".text-box",
-
-    {
-        opacity: 0.1,
-        y: -200,
-        textShadow: "#fff 0px 0px",
-    },
-    {
-        opacity: 1,
-        y: 0,
-        textShadow: "#000 3px 3px",
-        duration: 3,
-        scrollTrigger: {
-            trigger: ".text-box",
-            start: "200 100",
-            end: "1000",
-            scrub: false,
-            toggleActions: "play complete resume"
-        },
-
-    });
-
-let boxes = gsap.utils.toArray("section"),
-    container = document.querySelector("body"),
-    text = document.querySelector("#pin"),
-    padding = gsap.getProperty(container, "paddingTop", "px"),
-    // create a ScrollTrigger for each box that we can use to calculate snapping (we'll look at the "start" of each in the onRefresh)
-    snapTriggers = boxes.map(box => ScrollTrigger.create({
-        trigger: box,
-        start: "top " + padding + "px"
-    })),
-    snaps = []; // where we'll store the progress value for each box's ScrollTrigger (start)
-
-ScrollTrigger.create({
-    trigger: '#container',
-    markers: false,
-    pin: '#pin',
-    pinSpacing: false,
-    start: "top top",
-    end: () => "+=" + (boxes[boxes.length - 1].getBoundingClientRect().top),
-    onRefresh: self => {
-        // re-populate the "snaps" Array with the progress values for where each box hits the target spot.
-        let distance = self.end - self.start;
-        snapTriggers.forEach((trigger, i) => snaps[i] = ((trigger.start - self.start) / distance) - 0.0185);
-    },
-    snap: snaps
+    this.setTimeout(() => {
+        textBox.className += " fadeInTextClass"
+        bg.className += " scaleUpBgClass"
+        overlay.className += " scaleDownOverlayClass"
+    }, 500)
 });
 
 let burger = document.getElementById("burger");
@@ -105,14 +20,3 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("open");
     menu.classList.toggle("show");
 })
-
-navToIntro.addEventListener("click", () => {
-    gsap.to(window, {
-        scrollTo: {
-            y: 0,
-            autoKill: true
-        },
-        duration: 1
-    });
-})
-
